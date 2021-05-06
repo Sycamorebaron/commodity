940bfd4d90ec770afbbeaeb51223aaa35d2e2041
sycam <sycamorebaron@hotmail.com>
2021-05-06 17:41:57 +0800

    hf_rtn.py calculate hf rtn 30T

--- factor/test/factor_test_hf.py
+++ factor/test/factor_test_hf.py
@@ -10,13 +10,8 @@ from factor.test.factor_test import FactorTest
 pd.set_option('expand_frame_repr', False)
 pd.set_option('display.max_rows', 500)
 
-
-# 休息时间：10:15-10:30
-class HFRtn(FactorTest):
-    def __init__(self, factor_name, begin_date, end_date, init_cash, contract_list, local_data_path):
-        FactorTest.__init__(self, factor_name, begin_date, end_date, init_cash, contract_list, local_data_path)
-        self.rtn_30t = []
-
+class HFFactor(FactorTest):
+    # 休息时间：10:15-10:30
     def resample(self, data, rule):
         data = data.resample(on='datetime', rule=rule).agg(
             {
@@ -34,6 +29,12 @@ class HFRtn(FactorTest):
         data = data.loc[data['datetime'].apply(lambda x: (x.strftime('%H:%M') != '11:30') & (x.strftime('%H:%M') != '15:00'))]
 
         return data
+
+
+class HFRtn(HFFactor):
+    def __init__(self, factor_name, begin_date, end_date, init_cash, contract_list, local_data_path):
+        FactorTest.__init__(self, factor_name, begin_date, end_date, init_cash, contract_list, local_data_path)
+        self.rtn_30t = []
 
     def _daily_process(self):
         print(self.agent.earth_calender.now_date)
@@ -54,15 +55,6 @@ class HFRtn(FactorTest):
             open_comm_list.append(comm)
 
         daily_rtn_30t = self.t_daily_factor(open_comm_list)
-
-        # 每年保存数据
-        if (int(self.agent.earth_calender.now_date.strftime('%m')) >= 12) & (int(self.agent.earth_calender.now_date.strftime('%d')) >= 25):
-
-            rtn_30t = pd.DataFrame(self.rtn_30t)
-
-            rtn_30t.to_excel(
-                os.path.join(OUTPUT_DATA_PATH, '%s_std.xlsx' % self.agent.earth_calender.now_date.strftime('%Y'))
-            )
 
         self.rtn_30t.append(daily_rtn_30t)
 
@@ -85,9 +77,64 @@ class HFRtn(FactorTest):
                 else:
                     _rtn_df = data_30t
 
+        return _rtn_df
 
+
+class HFRtnMoment(HFFactor):
+    def __init__(self, factor_name, begin_date, end_date, init_cash, contract_list, local_data_path):
+        FactorTest.__init__(self, factor_name, begin_date, end_date, init_cash, contract_list, local_data_path)
+        self.factor_name = factor_name
+        self.mean = []
+        self.std = []
+        self.skew = []
+        self.kurt = []
+
+    def _daily_process(self):
+        print(self.agent.earth_calender.now_date)
+
+        open_comm_list = []
+
+        for comm in self.exchange.contract_dict.keys():
+            # 未上市的商品
+            if self.exchange.contract_dict[comm].first_listed_date > self.agent.earth_calender.now_date:
+                continue
+            # 已经退市的商品
+            if self.exchange.contract_dict[comm].last_de_listed_date < self.agent.earth_calender.now_date:
+                continue
+            print(comm)
+
+            self.exchange.contract_dict[comm].renew_main_sec_contract(now_date=self.agent.earth_calender.now_date)
+            self.exchange.contract_dict[comm].renew_operate_contract(now_date=self.agent.earth_calender.now_date)
+            open_comm_list.append(comm)
+
+        daily_mean, daily_std, daily_skew, daily_kurt = self.t_daily_factor(open_comm_list)
+
+        self.mean.append(daily_mean)
+        self.std.append(daily_std)
+        self.skew.append(daily_skew)
+        self.kurt.append(daily_kurt)
+
+    def t_daily_factor(self, open_comm_list):
+
+        _rtn_df = pd.DataFrame()
+        for comm in open_comm_list:
+            now_main_contract = self.exchange.contract_dict[comm].now_main_contract(
+                now_date=self.agent.earth_calender.now_date
+            )
+            _data = self.exchange.contract_dict[comm].data_dict[now_main_contract]
+            today_data = self.trunc_data(_data)
+            if len(today_data):
+                data_30t = self.resample(today_data.copy(), rule='30T')
+                data_30t['rtn'] = (data_30t['close'] / data_30t['open']) - 1
+                data_30t = data_30t[['datetime', 'rtn']]
+                data_30t.columns = ['datetime', comm]
+                if len(_rtn_df):
+                    _rtn_df = _rtn_df.merge(data_30t, on='datetime', how='outer')
+                else:
+                    _rtn_df = data_30t
 
         return _rtn_df
+
 
 
 if __name__ == '__main__':
