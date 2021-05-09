ce3703cf26e14cc533579e3b5e01aa6bbd989d8c
sycam <sycamorebaron@hotmail.com>
2021-05-09 14:32:17 +0800

    hf multi_process_cal_hf.py

--- factor/test/multi_process_cal_hf.py
+++ factor/test/multi_process_cal_hf.py
@@ -6,17 +6,24 @@ sys.path.append(BASE_DIR)
 from multiprocessing.pool import  Pool
 
 # =========30T
-from factor.test.cal_factor_realize_hf.hf_rtn import hf_rtn
+from factor.test.cal_factor_realize_hf.hf_big_factor import hf_big_factor
+from factor.test.cal_factor_realize_hf.hf_liquidity import hf_liquidity_pirce
+from factor.test.cal_factor_realize_hf.hf_pca import hf_pca_price
+from factor.test.cal_factor_realize_hf.hf_rtn_moment import hf_rtn_moment
+
+from factor.test.cal_factor_realize_hf.hf_singular import hf_singular
+from factor.test.cal_factor_realize_hf.hf_up_down import hf_up_down
+from factor.test.cal_factor_realize_hf.hf_vol_price import hf_vol_pirce
 
 
 if __name__ == '__main__':
 
     pool = Pool(processes=4)
 
-    pool.apply_async(hf_rtn, ('2011-01-01', '2013-12-31'))
-    pool.apply_async(hf_rtn, ('2014-01-01', '2016-12-31'))
-    pool.apply_async(hf_rtn, ('2017-01-01', '2019-12-31'))
-    pool.apply_async(hf_rtn, ('2020-01-01', '2021-02-28'))
+    pool.apply_async(hf_big_factor, ('2011-01-01', '2021-02-28'))
+    pool.apply_async(hf_liquidity_pirce, ('2011-01-01', '2021-02-28'))
+    pool.apply_async(hf_pca_price, ('2011-01-01', '2021-02-28'))
+    pool.apply_async(hf_rtn_moment, ('2011-01-01', '2021-02-28'))
 
     pool.close()
     pool.join()
