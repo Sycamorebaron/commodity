c8d7d1f33f3b6e7dcb5ca7256be0f6f5dade58b2
sycam <sycamorebaron@hotmail.com>
2021-08-19 08:20:52 +0800

    genetic factor sys path set

--- genetic_algo/genetic_utils.py
+++ genetic_algo/genetic_utils.py
@@ -1,2 +1,11 @@
-continuous_contract_path = r'D:\commodity\data\continuous_contract'
-factor_path = r'C:\1d_factor'
+import sys
+
+system = sys.platform
+
+if system.startswith('win'):
+    continuous_contract_path = r'D:\commodity\data\continuous_contract'
+    factor_path = r'C:\1d_factor'
+elif system.startswith('linux'):
+    continuous_contract_path = r'/home/sycamore/PycharmProjects/commodity/data/continuous_contract'
+    factor_path = r'/home/sycamore/1d_factor'
+
